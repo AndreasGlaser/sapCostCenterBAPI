e0a4e7f9feb20a454213420a51ffddf8341b9a93
AndreasGlaser <Andreasglaser7@gmail.com>
2018-11-06 14:38:44 +0100

    initialisierung

---
+++ sapmitDennis/sapmitDennis/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace sapmitDennis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(418, 175);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(46, 17);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(565, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(434, 264);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(46, 17);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl2;
+    }
+}
+
---
+++ sapmitDennis/sapmitDennis/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SAP.Middleware.Connector;
+namespace sapmitDennis
+{
+
+
+    public partial class Form1 : Form
+    {
+        RfcDestination rfcDestination=null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connector sapconnector = new connector();
+            RfcDestinationManager.RegisterDestinationConfiguration(sapconnector);
+            rfcDestination = null;
+            rfcDestination = RfcDestinationManager.GetDestination("costCenter");
+            try
+            {
+                if (rfcDestination != null)
+                {
+                    rfcDestination.Ping();
+                    lbl.Text = "conected";
+                }
+
+            }catch (Exception e1){
+                Console.WriteLine(e1.Message);
+                lbl.Text = "failed";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RfcRepository rfcRepository = rfcDestination.Repository;
+                var getList = rfcRepository.CreateFunction("BAPI_COSTCENTERGROUP_GETLIST");
+                getList.Invoke(rfcDestination);
+                getList.SetValue("CONTROLLINGAREAMASK", "0001");
+                getList.Invoke(rfcDestination);
+
+                var table = getList.GetTable("GROUPLIST");
+
+                for (int i = 0; i < table.RowCount; i++)
+                {
+                    lbl2.Text += table[i].GetString("DESCRIPT") + " || ";
+                }
+            }
+            catch (RfcCommunicationException ex)
+            {
+
+            }
+            catch (RfcLogonException ex)
+            {
+
+            }
+            catch (RfcAbapRuntimeException ex)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+
+
+        }
+    }
+}
