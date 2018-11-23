0997fc0e8dc085a43be375efe257158c4151f186
AndreasGlaser <Andreasglaser7@gmail.com>
2018-11-23 21:17:15 +0100

    abgabe hinzugefügt

---
+++ Abgabe/CostCenterGroup_Andreas_Glaser_WS2018/Projekt/CostCenterGroupBAPI/CostCenterGroup/Login.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CostCenterGroupBAPI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.connectionlbl = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Infolabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(93, 165);
+            this.loginbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(56, 19);
+            this.loginbtn.TabIndex = 1;
+            this.loginbtn.Text = "Login";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // connectionlbl
+            // 
+            this.connectionlbl.AutoSize = true;
+            this.connectionlbl.Location = new System.Drawing.Point(177, 165);
+            this.connectionlbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.connectionlbl.Name = "connectionlbl";
+            this.connectionlbl.Size = new System.Drawing.Size(0, 13);
+            this.connectionlbl.TabIndex = 2;
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(93, 64);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(100, 20);
+            this.NameBox.TabIndex = 3;
+            this.NameBox.Text = "IDES-006";
+            this.NameBox.TextChanged += new System.EventHandler(this.NameBox_TextChanged);
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(93, 112);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.PasswordChar = '*';
+            this.PassBox.Size = new System.Drawing.Size(100, 20);
+            this.PassBox.TabIndex = 4;
+            this.PassBox.Text = "6478921As";
+            this.PassBox.TextChanged += new System.EventHandler(this.PassBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // Infolabel
+            // 
+            this.Infolabel.AutoSize = true;
+            this.Infolabel.Location = new System.Drawing.Point(93, 195);
+            this.Infolabel.MaximumSize = new System.Drawing.Size(200, 0);
+            this.Infolabel.Name = "Infolabel";
+            this.Infolabel.Size = new System.Drawing.Size(0, 13);
+            this.Infolabel.TabIndex = 7;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 246);
+            this.Controls.Add(this.Infolabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.connectionlbl);
+            this.Controls.Add(this.loginbtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CostCenterGroupBAPI Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.Label connectionlbl;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Infolabel;
+    }
+}
---
+++ Abgabe/CostCenterGroup_Andreas_Glaser_WS2018/Projekt/CostCenterGroupBAPI/CostCenterGroup/Login.cs
@@ -0,0 +1,68 @@
+﻿using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CostCenterGroupBAPI
+{
+    public partial class Login : Form
+    {
+        static connector sapconnector = new connector();
+       
+        public Login()
+        {
+            InitializeComponent();
+           
+            RfcDestinationManager.RegisterDestinationConfiguration(sapconnector);
+            
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                Connection.rfcDestination = RfcDestinationManager.GetDestination("costCenter");
+                connectionlbl.Text = "connecting ...";
+                connectionlbl.BackColor = Color.Transparent;
+                this.Refresh();
+                   
+                Connection.rfcDestination.Ping();
+                connectionlbl.Text = "connected";
+                this.Hide();
+                new Overview().ShowDialog();
+                this.Close(); 
+            }
+            catch (SAP.Middleware.Connector.RfcLogonException ex)
+            {
+                Infolabel.Text = ex.Message;
+                Console.WriteLine(ex.Message);
+                connectionlbl.Text = "failed";
+                connectionlbl.BackColor = Color.Red;
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine(e1.Message);
+                connectionlbl.Text = "failed";
+                connectionlbl.BackColor = Color.Red;
+
+            }
+        }
+
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+            ConfigurationManager.AppSettings["SAP_USERNAME"] = NameBox.Text;
+        }
+
+        private void PassBox_TextChanged(object sender, EventArgs e)
+        {
+            ConfigurationManager.AppSettings["SAP_PASSWORD"] = PassBox.Text;
+        }
+    }
+}
